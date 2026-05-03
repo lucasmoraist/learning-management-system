873877bd13d9702c3bd8885a411a0b16b232165d
lucasmoraist <luksmnt1101@gmail.com>
2026-05-03 13:12:53 -0300

    feat: add IdentityPersistence class for managing Identity entities in the database

---
+++ src/main/java/com/lucasmoraist/lms/infrastructure/database/persistence/IdentityPersistence.java
@@ -0,0 +1,42 @@
+package com.lucasmoraist.lms.infrastructure.database.persistence;
+
+import com.lucasmoraist.lms.domain.model.Identity;
+import com.lucasmoraist.lms.infrastructure.database.entity.IdentityEntity;
+import com.lucasmoraist.lms.infrastructure.database.repository.IdentityRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Slf4j
+@Component
+public class IdentityPersistence {
+
+    private final IdentityRepository identityRepository;
+    private final ModelMapper modelMapper;
+
+    public IdentityPersistence(IdentityRepository identityRepository, ModelMapper modelMapper) {
+        this.identityRepository = identityRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Transactional
+    public Identity save(Identity identity) {
+        IdentityEntity identityEntity = this.modelMapper.map(identity, IdentityEntity.class);
+
+        if (identityEntity.getProfile() != null) {
+            identityEntity.getProfile().setIdentity(identityEntity);
+        }
+
+        IdentityEntity identityEntitySaved = this.identityRepository.saveAndFlush(identityEntity);
+        return this.modelMapper.map(identityEntitySaved, Identity.class);
+    }
+
+    public Optional<Identity> findByEmail(String email) {
+        Optional<IdentityEntity> identityEntityOptional = this.identityRepository.findByEmail(email);
+        return identityEntityOptional.map(identityEntity -> this.modelMapper.map(identityEntity, Identity.class));
+    }
+
+}
