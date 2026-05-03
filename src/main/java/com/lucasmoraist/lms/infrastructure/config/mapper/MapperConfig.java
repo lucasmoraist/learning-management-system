69c1da162460fe0aaf9b53366fcc4e604f685ce0
lucasmoraist <luksmnt1101@gmail.com>
2026-05-03 13:13:05 -0300

    feat: add MapperConfig class for configuring ModelMapper bean

---
+++ src/main/java/com/lucasmoraist/lms/infrastructure/config/mapper/MapperConfig.java
@@ -0,0 +1,21 @@
+package com.lucasmoraist.lms.infrastructure.config.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+
+        return modelMapper;
+    }
+
+}
