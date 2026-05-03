2bfda6395f3206da77feb57f9f7afc24dd4abdd5
lucasmoraist <luksmnt1101@gmail.com>
2026-05-03 14:18:09 -0300

    feat: add CreateUserDTO for user creation data transfer

---
+++ src/main/java/com/lucasmoraist/lms/adapter/web/dto/user/CreateUserDTO.java
@@ -0,0 +1,27 @@
+package com.lucasmoraist.lms.adapter.web.dto.user;
+
+import com.lucasmoraist.lms.domain.enums.RoleType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public record CreateUserDTO(
+        @NotBlank(message = "Name is required")
+        @Size(min = 3, max = 255, message = "Name must be between 3 and 100 characters")
+        String name,
+        @NotNull(message = "Birth date is required")
+        LocalDate birthDate,
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email should be valid")
+        String email,
+        @NotBlank(message = "Password is required")
+        @Size(min = 6, max = 255, message = "Password must be at least 6 characters long")
+        String password,
+        @NotNull(message = "Role is required")
+        RoleType role
+) {
+
+}
