fd73349b56cacb945491f99e7456fe8935dddba1
lucasmoraist <luksmnt1101@gmail.com>
2026-05-04 00:09:49 -0300

    feat: add exception handling for Authentication, Certificate, and Token exceptions in RestExceptionHandler

--- src/main/java/com/lucasmoraist/lms/adapter/web/handler/RestExceptionHandler.java
+++ src/main/java/com/lucasmoraist/lms/adapter/web/handler/RestExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.lucasmoraist.lms.adapter.web.handler;
 
 import com.lucasmoraist.lms.adapter.web.handler.model.DataValidationException;
+import com.lucasmoraist.lms.adapter.web.handler.model.ExceptionDTO;
+import com.lucasmoraist.lms.domain.exceptions.AuthenticationException;
+import com.lucasmoraist.lms.domain.exceptions.CertificateException;
+import com.lucasmoraist.lms.domain.exceptions.TokenException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -21,6 +26,30 @@ public class RestExceptionHandler {
                 .toList();
         log.warn("Message: {} - ", ex.getMessage(), ex);
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    protected ResponseEntity<ExceptionDTO> handleAuthenticationException(AuthenticationException ex) {
+        log.warn("Message: {} - ", ex.getMessage(), ex);
+        return ResponseEntity.status(401).body(new ExceptionDTO(ex.getMessage(), HttpStatus.UNAUTHORIZED));
+    }
+
+    @ExceptionHandler(CertificateException.class)
+    protected ResponseEntity<Void> handleCertificateException(CertificateException ex) {
+        log.warn("Message: {} - ", ex.getMessage(), ex);
+        return ResponseEntity.status(503).build();
+    }
+
+    @ExceptionHandler(TokenException.class)
+    protected ResponseEntity<ExceptionDTO> handleTokenException(TokenException ex) {
+        log.warn("Message: {} - ", ex.getMessage(), ex);
+        return ResponseEntity.status(401).body(new ExceptionDTO(ex.getMessage(), HttpStatus.UNAUTHORIZED));
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<Void> handleGenericException(Exception ex) {
+        log.error("Message: {} - ", ex.getMessage(), ex);
+        return ResponseEntity.internalServerError().build();
     }
 
 }
