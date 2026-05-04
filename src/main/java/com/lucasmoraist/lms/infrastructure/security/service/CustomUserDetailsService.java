7253ed897039b1518fc43a41c8f2007edc715fff
lucasmoraist <luksmnt1101@gmail.com>
2026-05-03 23:41:15 -0300

    feat: implement CustomUserDetailsService for user authentication

---
+++ src/main/java/com/lucasmoraist/lms/infrastructure/security/service/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.lucasmoraist.lms.infrastructure.security.service;
+
+import com.lucasmoraist.lms.domain.model.Identity;
+import com.lucasmoraist.lms.infrastructure.database.persistence.IdentityPersistence;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final IdentityPersistence identityPersistence;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Identity user = this.identityPersistence.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(it -> authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRoles())));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+    }
+
+}
