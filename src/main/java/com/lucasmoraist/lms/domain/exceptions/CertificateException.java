7b588a644c06c0775b9f02de69f0d5e17f4b7b3d
lucasmoraist <luksmnt1101@gmail.com>
2026-05-03 23:27:32 -0300

    feat: add CertificateException class for handling certificate-related errors

---
+++ src/main/java/com/lucasmoraist/lms/domain/exceptions/CertificateException.java
@@ -0,0 +1,9 @@
+package com.lucasmoraist.lms.domain.exceptions;
+
+public class CertificateException extends RuntimeException {
+
+    public CertificateException(String message, Throwable ex) {
+        super(message, ex);
+    }
+
+}
