6b93cc6d90cf810aea677b28cd10917c7df60869
lucasmoraist <luksmnt1101@gmail.com>
2026-05-03 23:28:03 -0300

    feat: add Token class for managing access token details

---
+++ src/main/java/com/lucasmoraist/lms/domain/model/Token.java
@@ -0,0 +1,18 @@
+package com.lucasmoraist.lms.domain.model;
+
+import lombok.Getter;
+
+@Getter
+public class Token {
+
+    private final String accessToken;
+    private final Integer expiresIn;
+    private final String type;
+
+    public Token(String accessToken, Integer expiresIn) {
+        this.accessToken = accessToken;
+        this.expiresIn = expiresIn;
+        this.type = "Bearer";
+    }
+
+}
