ae79d0de065a3a6fc140527ce39623d895f6e33e
lucasmoraist <luksmnt1101@gmail.com>
2026-05-05 00:45:56 -0300

    feat: add validation annotations to UpdateUserDTO fields

--- src/main/java/com/lucasmoraist/lms/adapter/web/dto/user/UpdateUserDTO.java
+++ src/main/java/com/lucasmoraist/lms/adapter/web/dto/user/UpdateUserDTO.java
@@ -1,5 +1,8 @@
 package com.lucasmoraist.lms.adapter.web.dto.user;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -9,22 +12,29 @@ import java.util.List;
 @ToString
 public class UpdateUserDTO {
 
+    @Email(message = "Invalid email format")
     private String email;
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
+    @Valid
     private ProfileUpdateDTO profile;
 
     @Getter
     @ToString
     public static class ProfileUpdateDTO {
+        @Size(min = 2, max = 255, message = "Name must be between 3 and 255 characters")
         private String name;
         private String bio;
+        @Valid
         private List<SocialLinkUpdateDTO> socialLinks;
     }
 
     @Getter
     @ToString
     public static class SocialLinkUpdateDTO {
+        @Size(max = 100, message = "Name must be less than 100 characters")
         private String name;
+        @Size(max = 255, message = "Link must be less than 255 characters")
         private String link;
     }
 
