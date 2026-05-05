d00677844a1e4c533dc4418f0d62c644af36e49e
lucasmoraist <luksmnt1101@gmail.com>
2026-05-04 23:49:11 -0300

    refactor: change findDocumentByValue method to static and return DocumentType directly

--- src/main/java/com/lucasmoraist/lms/domain/enums/DocumentType.java
+++ src/main/java/com/lucasmoraist/lms/domain/enums/DocumentType.java
@@ -1,27 +1,21 @@
 package com.lucasmoraist.lms.domain.enums;
 
 public enum DocumentType {
-    CPF("CPF"),
-    RG("RG"),
-    CNPJ("CNPJ");
+    CPF,
+    RG,
+    CNPJ;
 
-    private final String value;
-
-    DocumentType(String value) {
-        this.value = value;
-    }
-
-    public String findDocumentByValue(String value) {
+    public static DocumentType findDocumentByValue(String value) {
         final String cpfRegex = "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}";
         final String rgRegex = "\\d{2}\\.\\d{3}\\.\\d{3}-\\d{1}";
         final String cnpjRegex = "\\d{2}\\.\\d{3}\\.\\d{3}/\\d{4}-\\d{2}";
 
         if (value.matches(cpfRegex)) {
-            return CPF.value;
+            return CPF;
         } else if (value.matches(rgRegex)) {
-            return RG.value;
+            return RG;
         } else if (value.matches(cnpjRegex)) {
-            return CNPJ.value;
+            return CNPJ;
         } else {
             throw new IllegalArgumentException("Invalid document format: " + value);
         }
