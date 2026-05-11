f58d8484941c4d0bd7b45e6c0b333fd580460d0d
lucasmoraist <luksmnt1101@gmail.com>
2026-05-11 20:24:31 -0300

    feat: update DocumentEntity to use UUID generation strategy for id

--- src/main/java/com/lucasmoraist/lms/infrastructure/database/entity/DocumentEntity.java
+++ src/main/java/com/lucasmoraist/lms/infrastructure/database/entity/DocumentEntity.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -22,7 +24,7 @@ import java.util.UUID;
 @Entity(name = "tb_document")
 public class DocumentEntity {
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     @Enumerated(EnumType.STRING)
     private DocumentType type;
