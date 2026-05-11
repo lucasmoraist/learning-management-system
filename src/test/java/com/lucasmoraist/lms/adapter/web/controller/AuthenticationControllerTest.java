1645cc5389f5e44f0903fee0f016c62f8d321305
lucasmoraist <luksmnt1101@gmail.com>
2026-05-10 23:22:50 -0300

    test: add unit tests for error handling in AuthenticationController

--- src/test/java/com/lucasmoraist/lms/adapter/web/controller/AuthenticationControllerTest.java
+++ src/test/java/com/lucasmoraist/lms/adapter/web/controller/AuthenticationControllerTest.java
@@ -2,6 +2,8 @@ package com.lucasmoraist.lms.adapter.web.controller;
 
 import com.lucasmoraist.lms.application.usecases.authentication.GenerateTokenCase;
 import com.lucasmoraist.lms.domain.exceptions.AuthenticationException;
+import com.lucasmoraist.lms.domain.exceptions.CertificateException;
+import com.lucasmoraist.lms.domain.exceptions.TokenException;
 import com.lucasmoraist.lms.domain.gateway.TokenGateway;
 import com.lucasmoraist.lms.domain.model.Token;
 import com.lucasmoraist.lms.infrastructure.security.service.CustomUserDetailsService;
@@ -76,6 +78,71 @@ class AuthenticationControllerTest {
                         .content(dtoJson))
                 .andExpect(status().isUnauthorized())
                 .andExpect(jsonPath("message").value("Invalid email or password"));
+
+        verify(generateTokenCase, times(1)).execute(any(), any());
+    }
+
+    @Test
+    @DisplayName("Should return 503 when certificate error occurs")
+    void case03() throws Exception {
+        final String dtoJson = """
+                {
+                  "email": "johndoe@email.com",
+                  "password": "password123"
+                }
+                """;
+
+        when(generateTokenCase.execute(any(), any())).thenThrow(new CertificateException("Certificate error",
+                new RuntimeException("Certificate error")));
+
+        mockMvc.perform(post("/api/v1/auth")
+                        .contentType("application/json")
+                        .with(jwt())
+                        .content(dtoJson))
+                .andExpect(status().is(503));
+
+        verify(generateTokenCase, times(1)).execute(any(), any());
+    }
+
+    @Test
+    @DisplayName("Should return 401 when certificate error occurs")
+    void case04() throws Exception {
+        final String dtoJson = """
+                {
+                  "email": "johndoe@email.com",
+                  "password": "password123"
+                }
+                """;
+
+        when(generateTokenCase.execute(any(), any())).thenThrow(new TokenException("Token error",
+                new RuntimeException("Token error")));
+
+        mockMvc.perform(post("/api/v1/auth")
+                        .contentType("application/json")
+                        .with(jwt())
+                        .content(dtoJson))
+                .andExpect(status().isUnauthorized());
+
+        verify(generateTokenCase, times(1)).execute(any(), any());
+    }
+
+    @Test
+    @DisplayName("Should return 500 when unexpected error occurs")
+    void case05() throws Exception {
+        final String dtoJson = """
+                {
+                  "email": "johndoe@email.com",
+                  "password": "password123"
+                }
+                """;
+
+        when(generateTokenCase.execute(any(), any())).thenThrow(new RuntimeException("Unexpected error"));
+
+        mockMvc.perform(post("/api/v1/auth")
+                        .contentType("application/json")
+                        .with(jwt())
+                        .content(dtoJson))
+                .andExpect(status().isInternalServerError());
 
         verify(generateTokenCase, times(1)).execute(any(), any());
     }
