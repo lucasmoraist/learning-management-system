4a16636d5e56630f15a78bf4359e502312f8490e
lucasmoraist <luksmnt1101@gmail.com>
2026-05-19 21:51:57 -0300

    refact: update SQL scripts to use double quotes for column names and rename lesson table

--- src/main/resources/db/migration/V11__create-table-module.sql
+++ src/main/resources/db/migration/V11__create-table-module.sql
@@ -2,6 +2,6 @@ CREATE TABLE tb_module (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     course_id UUID NOT NULL,
     title VARCHAR(255) NOT NULL,
-    'position' INT NOT NULL, -- Para ordenar os módulos
+    "position" INT NOT NULL, -- Para ordenar os módulos
     CONSTRAINT fk_module_course FOREIGN KEY (course_id) REFERENCES tb_course(id) ON DELETE CASCADE
 );
--- src/main/resources/db/migration/V12__create-table-lesson.sql
+++ src/main/resources/db/migration/V12__create-table-lesson.sql
@@ -3,7 +3,7 @@ CREATE TABLE tb_lesson (
     module_id UUID NOT NULL,
     title VARCHAR(255) NOT NULL,
     content_url TEXT, -- Link para S3/Vimeo/Youtube
-    'position' INT NOT NULL, -- Para ordenar as aulas dentro do módulo
+    "position" INT NOT NULL, -- Para ordenar as aulas dentro do módulo
     duration_in_seconds INT,
     CONSTRAINT fk_lesson_module FOREIGN KEY (module_id) REFERENCES tb_module(id) ON DELETE CASCADE
 );
