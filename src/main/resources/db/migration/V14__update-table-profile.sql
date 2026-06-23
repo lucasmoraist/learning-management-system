41ede551412cf32d2a9841354141abd728be0293
lucasmoraist <luksmnt1101@gmail.com>
2026-06-23 20:43:45 -0300

    feat: remove NOT NULL constraint on subscription_id in tb_profile for data flexibility

--- src/main/resources/db/migration/V14__update-table-profile.sql
+++ src/main/resources/db/migration/V14__update-table-profile.sql
@@ -3,9 +3,6 @@ ALTER TABLE tb_profile ADD COLUMN subscription_id UUID UNIQUE;
 
 -- [OPCIONAL] Associe ou crie assinaturas existentes para os perfis atuais aqui através de um UPDATE
 
--- 2. Torna a coluna obrigatória após popular os dados
-ALTER TABLE tb_profile ALTER COLUMN subscription_id SET NOT NULL;
-
 -- 3. Adiciona a chave estrangeira
 ALTER TABLE tb_profile
 ADD CONSTRAINT fk_profile_subscription
