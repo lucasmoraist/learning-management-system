51931dd22850abd21f8466359b95260792618dcc
lucasmoraist <luksmnt1101@gmail.com>
2026-06-27 00:35:46 -0300

    feat: add JsonDeserialize annotation to SubscriptionDTO for improved serialization

--- src/main/java/com/lucasmoraist/lms/adapter/web/dto/payment/SubscriptionDTO.java
+++ src/main/java/com/lucasmoraist/lms/adapter/web/dto/payment/SubscriptionDTO.java
@@ -1,8 +1,10 @@
 package com.lucasmoraist.lms.adapter.web.dto.payment;
 
+import tools.jackson.databind.annotation.JsonDeserialize;
 import tools.jackson.databind.annotation.JsonSerialize;
 
 @JsonSerialize(as = CreditSubscriptionDTO.class)
+@JsonDeserialize(as = CreditSubscriptionDTO.class)
 public sealed interface SubscriptionDTO permits CreditSubscriptionDTO {
 
 }
