ebf6d7b50b8cd7c068f31bb0a97d716f99b367a6
lucasmoraist <luksmnt1101@gmail.com>
2026-06-28 11:20:37 -0300

    feat: update SecurityConfig to enhance role-based access control for user and payment endpoints

--- src/main/java/com/lucasmoraist/lms/infrastructure/security/config/SecurityConfig.java
+++ src/main/java/com/lucasmoraist/lms/infrastructure/security/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.lucasmoraist.lms.infrastructure.security.config;
 
 import com.lucasmoraist.lms.adapter.web.filter.SecurityFilter;
+import com.lucasmoraist.lms.domain.enums.RoleType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -43,11 +44,35 @@ public class SecurityConfig {
                 .sessionManagement(s ->
                         s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+
+                        // 1. Endpoints Públicos (Monitoramento e Documentação)
                         .requestMatchers(HEALTH_ENDPOINTS).permitAll()
                         .requestMatchers(SWAGGER_ENDPOINTS).permitAll()
+
+                        // 2. Endpoints de Autenticação e Registro Inicial
                         .requestMatchers(HttpMethod.POST, "/api/v1/users/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/v1/users/list").hasRole("ADMIN")
+
+                        // 3. Controle de Usuários (UserController)
+                        .requestMatchers(HttpMethod.GET, "/api/v1/users/list").hasRole(RoleType.ADMIN.name())
+                        .requestMatchers("/api/v1/users/me/**").authenticated() // /me, /me/update, /me/delete
+
+                        // 4. Fluxo de Assinatura e Pagamentos (PaymentController)
+                        .requestMatchers(HttpMethod.POST, "/api/v1/payment/subscribe").hasRole(RoleType.USER.name())
+                        .requestMatchers(HttpMethod.PATCH, "/api/v1/payment/*/cancel").hasAnyRole(RoleType.SUBSCRIBER.name(), RoleType.ADMIN.name())
+
+                        // 5. Criação e Edição de Catálogo (Cursos, Módulos e Lições)
+                        .requestMatchers(HttpMethod.POST, "/api/v1/courses/create").hasAnyRole(RoleType.INSTRUCTOR.name(),
+                                RoleType.ADMIN.name())
+                        .requestMatchers(HttpMethod.POST, "/api/v1/modules/add/**").hasAnyRole(RoleType.INSTRUCTOR.name(), RoleType.ADMIN.name())
+                        .requestMatchers(HttpMethod.POST, "/api/v1/lessons/add/module/**").hasAnyRole(RoleType.INSTRUCTOR.name(), RoleType.ADMIN.name())
+
+                        // 6. Consumo de Conteúdo (Visualizar cursos e assistir lições)
+                        // Exige que seja um Assinante, Instrutor ou Admin
+                        .requestMatchers(HttpMethod.GET, "/api/v1/courses/**").hasAnyRole(RoleType.SUBSCRIBER.name(), RoleType.INSTRUCTOR.name(), RoleType.ADMIN.name())
+                        .requestMatchers(HttpMethod.GET, "/api/v1/lessons/**").hasAnyRole(RoleType.SUBSCRIBER.name(), RoleType.INSTRUCTOR.name(), RoleType.ADMIN.name())
+
+                        // Qualquer
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
