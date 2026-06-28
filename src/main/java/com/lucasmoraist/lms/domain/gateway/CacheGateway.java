4149a5a363141988e15599c60d602e86ffb5a987
lucasmoraist <luksmnt1101@gmail.com>
2026-06-28 13:33:10 -0300

    feat: enhance CacheGateway with get and set methods for improved caching functionality

--- src/main/java/com/lucasmoraist/lms/domain/gateway/CacheGateway.java
+++ src/main/java/com/lucasmoraist/lms/domain/gateway/CacheGateway.java
@@ -1,8 +1,12 @@
 package com.lucasmoraist.lms.domain.gateway;
 
+import java.util.Optional;
+
 public interface CacheGateway {
 
     Boolean setIfAbsent(String key, Object value, long timeoutInSeconds);
     void delete(String key);
+    Optional<Object> get(String cacheKey, Class<?> clazzType);
+    void set(String cacheKey, Object value, int ttlInSeconds);
 
 }
--- src/main/java/com/lucasmoraist/lms/infrastructure/cache/RedisCacheImpl.java
+++ src/main/java/com/lucasmoraist/lms/infrastructure/cache/RedisCacheImpl.java
@@ -1,20 +1,25 @@
 package com.lucasmoraist.lms.infrastructure.cache;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lucasmoraist.lms.domain.gateway.CacheGateway;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.util.Optional;
 
 @Slf4j
 @Component
 public class RedisCacheImpl implements CacheGateway {
 
     private final RedisTemplate<String, Object> redisTemplate;
+    private final ModelMapper modelMapper;
 
-    public RedisCacheImpl(RedisTemplate<String, Object> redisTemplate) {
+    public RedisCacheImpl(RedisTemplate<String, Object> redisTemplate, ModelMapper modelMapper) {
         this.redisTemplate = redisTemplate;
+        this.modelMapper = modelMapper;
     }
 
     @Override
@@ -29,6 +34,33 @@ public class RedisCacheImpl implements CacheGateway {
     @Override
     public void delete(String key) {
         redisTemplate.delete(key);
+    }
+
+    @Override
+    public Optional<Object> get(String cacheKey, Class<?> clazzType) {
+        Object value = redisTemplate.opsForValue().get(cacheKey);
+        if (value == null) {
+            return Optional.empty();
+        } else if (clazzType.isInstance(value)) {
+            return Optional.of(value);
+        } else {
+            try {
+                Object deserializedValue = this.modelMapper.map(value, clazzType);
+                return Optional.of(deserializedValue);
+            } catch (IllegalArgumentException e) {
+                log.error("Failed to deserialize value for key {}: {}", cacheKey, e.getMessage());
+                return Optional.empty();
+            }
+        }
+    }
+
+    @Override
+    public void set(String cacheKey, Object value, int ttlInSeconds) {
+        if (ttlInSeconds > 0) {
+            redisTemplate.opsForValue().set(cacheKey, value, Duration.ofSeconds(ttlInSeconds));
+        } else {
+            redisTemplate.opsForValue().set(cacheKey, value);
+        }
     }
 
 }
