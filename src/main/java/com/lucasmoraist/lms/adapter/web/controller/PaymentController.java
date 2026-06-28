8ba617f64b27910a87f38514922f6fee824e85c6
lucasmoraist <luksmnt1101@gmail.com>
2026-06-28 10:49:27 -0300

    feat: add cancel subscription endpoint to PaymentController

--- src/main/java/com/lucasmoraist/lms/adapter/web/controller/PaymentController.java
+++ src/main/java/com/lucasmoraist/lms/adapter/web/controller/PaymentController.java
@@ -1,6 +1,7 @@
 package com.lucasmoraist.lms.adapter.web.controller;
 
 import com.lucasmoraist.lms.adapter.web.dto.payment.CreateSubscriptionDTO;
+import com.lucasmoraist.lms.application.usecases.payment.CancelSubscriptionCase;
 import com.lucasmoraist.lms.application.usecases.payment.CreateSubscriptionCase;
 import com.lucasmoraist.lms.application.utils.TraceIdUtils;
 import com.lucasmoraist.lms.domain.model.payment.PaymentResult;
@@ -8,6 +9,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -23,6 +27,7 @@ import java.net.URI;
 public class PaymentController {
 
     private final CreateSubscriptionCase createSubscriptionCase;
+    private final CancelSubscriptionCase cancelSubscriptionCase;
 
     @PostMapping("/subscribe")
     public ResponseEntity<PaymentResult> subscribe(@RequestHeader("Authorization") String authorization,
@@ -34,6 +39,16 @@ public class PaymentController {
         URI location = URI.create("/api/v1/auth/refresh-role");
 
         return ResponseEntity.created(location).body(result);
+    }
+
+    @PatchMapping("/{subscriptionId}/cancel")
+    public ResponseEntity<Void> cancelSubscription(@PathVariable UUID subscriptionId,
+                                                   @RequestHeader("Authorization") String authorization) {
+        String traceId = TraceIdUtils.generateTraceId();
+        log.info("[{}] - Cancelling subscription for user with payment method {}", traceId, subscriptionId);
+        
+        this.cancelSubscriptionCase.execute(subscriptionId, authorization, traceId);
+        return ResponseEntity.noContent().build();
     }
 
 }
